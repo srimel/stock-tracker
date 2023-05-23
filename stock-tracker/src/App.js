c95de095faee4921377be0951e234888cc323917
Stuart Rimel <stuart.rimel@gmail.com>
2023-05-23 16:31:05 -0700

    delete generated create-react-app fluff

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Site Under Construction...</h1>
+      <img
+        src="https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif"
+        alt="octopus laying bricks"
+      />
     </div>
   );
 }
