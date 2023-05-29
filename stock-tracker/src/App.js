9c34cf114b69889d0fe4f9c3db39df7143736063
Stuart Rimel <stuart.rimel@gmail.com>
2023-05-29 11:09:12 -0700

    create 3 charts within app.js

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import CandleStick from './components/chart/CandleStick';
 
 function App() {
   return (
     <div className="App">
       <h1>Site Under Construction...</h1>
-      <img
-        src="https://media.giphy.com/media/fVeAI9dyD5ssIFyOyM/giphy.gif"
-        alt="octopus laying bricks"
-      />
+      <div className="chart-container">
+        <CandleStick symbol1="AAPL" />
+        <CandleStick symbol1="TSLA" />
+        <CandleStick symbol1="AMZN" />
+      </div>
     </div>
   );
 }
