a2e0711ec92dc218a072a4d40599d20e927bafea
Jacob Bentley <jpb7@pdx.edu>
2023-05-30 13:29:22 -0700

    Try to get API data in App component using client

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
 import './App.css';
-//import TickerWebSocket from './Ticker';
-
-const finnhub = require('finnhub');
-
-const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = process.env.REACT_APP_API_KEY;
-const finnhubClient = new finnhub.DefaultApi();
-
-// Tick Data
-finnhubClient.stockTick("AAPL", "2020-03-25", 500, 0, (error, data, response) => {
-    console.log(data);
-});
 
 const App = () => {
+  const finnhub = require('finnhub');
+
+  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+  api_key.apiKey = process.env.REACT_APP_API_KEY;
+  const finnhubClient = new finnhub.DefaultApi();
+
+  // Tick Data
+  finnhubClient.stockTick("AAPL", "2020-03-25", 500, 0, (error, data, response) => {
+      console.log(data);
+  });
+
   return (
     <div className="App">
       <h1>Stock Tracker</h1>
