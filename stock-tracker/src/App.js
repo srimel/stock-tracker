b6b8bc522494d0edc1ab70721d905bee5a053e4d
Jacob Bentley <jpb7@pdx.edu>
2023-05-30 18:16:41 -0700

    Remove old code

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -2,17 +2,6 @@ import './App.css';
 import Ticker from './Ticker';
 
 const App = () => {
-  const finnhub = require('finnhub');
-
-  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-  api_key.apiKey = process.env.REACT_APP_API_KEY;
-  const finnhubClient = new finnhub.DefaultApi();
-
-  // Tick Data
-  finnhubClient.stockTick("AAPL", "2020-03-25", 500, 0, (error, data, response) => {
-      console.log(data);
-  });
-
   return (
     <div className="App">
       <h1>Stock Tracker</h1>
