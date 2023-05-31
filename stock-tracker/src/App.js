f0f0a884d2adaf35350166e76baaa9b5e3ed0cad
Stuart Rimel <stuart.rimel@gmail.com>
2023-05-31 03:07:44 -0700

    WIP create homepage, navigation routing

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import CandleStick from './components/chart/CandleStick';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import { Routes, Route } from 'react-router-dom';
+
+// The Header component will persist on all pages, so it will be placed in the App component.
+// The header component will contain the site title and navigation links as well as the stock ticker.
 
 function App() {
   return (
     <div className="App">
-      <h1>Site Under Construction...</h1>
-      <div className="chart-container">
-        <CandleStick symbol1="AAPL" symbol2="GOOGL" />
-        <CandleStick symbol1="TSLA" />
-        <CandleStick symbol1="AMZN" />
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/comparison" element={<h1>Comparison</h1>} />
+        <Route path="/news" element={<h1>News</h1>} />
+      </Routes>
     </div>
   );
 }
---
+++ stock-tracker/src/components/navigation/Navigation.js
@@ -0,0 +1,26 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto mx-auto">
+            <Nav.Link as={NavLink} to="/" exact>
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/comparison">
+              Comparison
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/news">
+              News
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
