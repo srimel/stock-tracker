dc2908792f180515ddb0217bd76fb3e1e46e4367
Richard Duffy <rduffy@pdx.edu>
2023-06-02 15:00:04 -0700

    cant get graphs to display on compare page

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import CandleStick from './components/chart/CandleStick';
+import StockPrices from './StockPrices';
 
 function App() {
   return (
     <div className="App">
-      <h1>Site Under Construction...</h1>
-      <div className="chart-container">
-        <CandleStick symbol1="AAPL" symbol2="GOOGL" />
-        <CandleStick symbol1="TSLA" />
-        <CandleStick symbol1="AMZN" />
-      </div>
+      <StockPrices />
     </div>
+    /*
+    <div className="chart-container">
+    <CandleStick symbol1="AAPL" symbol2="GOOGL" />
+    <CandleStick symbol1="TSLA" />
+    <CandleStick symbol1="AMZN" />
+  </div> 
+  */
   );
 }
 
