ac73e319c0b2deaba3c69cadd94ace735207927a
Stuart Rimel <stuart.rimel@gmail.com>
2023-06-03 20:12:04 -0700

    generate date range 1 month from current date as default date range for API call

--- stock-tracker/src/components/news/News.js
+++ stock-tracker/src/components/news/News.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
+// Defaults to get one month of company news
+
 function News(props) {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchNews = async () => {
+      const today = new Date();
+      const monthAgo = new Date();
+      monthAgo.setMonth(today.getMonth() - 1);
+
+      const formatDate = (date) => {
+        const year = date.getFullYear();
+        let month = date.getMonth() + 1; // months are zero indexed
+        let day = date.getDate();
+
+        // prepend month or day with zero if single digit
+        if (month < 10) {
+          month = `0${month}`;
+        }
+        if (day < 10) {
+          day = `0${day}`;
+        }
+
+        return `${year}-${month}-${day}`;
+      };
+
+      console.log('today', formatDate(today));
+      console.log('monthAgo', formatDate(monthAgo));
+
       const response = await fetch(
         `https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2023-01-01&to=2023-05-01&token=${process.env.REACT_APP_FINNHUB_API_KEY}`,
       );
@@ -33,7 +58,7 @@ function News(props) {
           </div>
         ))
       ) : (
-        <p>Coming soon...</p>
+        <p>Failed to load news data...</p>
       )}
     </div>
   );
