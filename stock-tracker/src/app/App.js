02d2b162fc5a88b022897463609e561c341130af
Stuart Rimel <stuart.rimel@gmail.com>
2023-06-04 15:10:27 -0700

    apply bootstrap styles to news page

--- stock-tracker/src/app/App.js
+++ stock-tracker/src/app/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Header from '../components/header/Header';
 import Home from '../components/home/Home';
-import { Routes, Route } from 'react-router-dom';
 import News from '../components/news/News';
+import { Routes, Route } from 'react-router-dom';
 
 // The Header component will persist on all pages, so it will be placed in the App component.
 // The header component will contain the site title and navigation links as well as the stock ticker.
--- stock-tracker/src/components/news/News.js
+++ stock-tracker/src/components/news/News.js
@@ -1,65 +1,97 @@
 import React, { useState, useEffect } from 'react';
-
-// Defaults to get one month of company news
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function News(props) {
   const [news, setNews] = useState([]);
+  const [search, setSearch] = useState('');
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const today = new Date();
-      const monthAgo = new Date();
-      monthAgo.setMonth(today.getMonth() - 1);
+  const fetchNews = async (symbol) => {
+    const today = new Date();
+    const monthAgo = new Date();
+    monthAgo.setMonth(today.getMonth() - 1);
 
-      const formatDate = (date) => {
-        const year = date.getFullYear();
-        let month = date.getMonth() + 1; // months are zero indexed
-        let day = date.getDate();
+    const formatDate = (date) => {
+      const year = date.getFullYear();
+      let month = date.getMonth() + 1; // months are zero indexed
+      let day = date.getDate();
 
-        // prepend month or day with zero if single digit
-        if (month < 10) {
-          month = `0${month}`;
-        }
-        if (day < 10) {
-          day = `0${day}`;
-        }
-
-        return `${year}-${month}-${day}`;
-      };
-
-      console.log('today', formatDate(today));
-      console.log('monthAgo', formatDate(monthAgo));
-
-      const response = await fetch(
-        `https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2023-01-01&to=2023-05-01&token=${process.env.REACT_APP_FINNHUB_API_KEY}`,
-      );
-
-      if (!response.ok) {
-        console.error(`HTTP error! status: ${response.status}`);
-        return;
+      // prepend month or day with zero if single digit
+      if (month < 10) {
+        month = `0${month}`;
+      }
+      if (day < 10) {
+        day = `0${day}`;
       }
 
-      const data = await response.json();
-      console.log('news', data);
-      setNews(data);
+      return `${year}-${month}-${day}`;
     };
 
-    fetchNews();
-  }, []);
+    console.log('today', formatDate(today));
+    console.log('monthAgo', formatDate(monthAgo));
+
+    const response = await fetch(
+      `https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=${formatDate(
+        monthAgo,
+      )}&to=${formatDate(today)}&token=${
+        process.env.REACT_APP_FINNHUB_API_KEY
+      }`,
+    );
+
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
+      return;
+    }
+
+    const data = await response.json();
+    console.log('news', data);
+    setNews(data);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetchNews(search);
+  };
 
   return (
     <div className="news">
-      <h1>Financial News Hub</h1>
-      {news.length ? (
-        news.map((item, index) => (
-          <div key={index}>
-            <h2>{item.headline}</h2>
-            <p>{item.summary}</p>
+      <Container className="mt-0">
+        <Form className="mb-4" onSubmit={handleSearchSubmit}>
+          <InputGroup className="w-25 mx-auto">
+            <Form.Control
+              type="text"
+              value={search}
+              onChange={handleSearchChange}
+              placeholder="Search by company symbol..."
+            />
+            <Button type="submit">Search</Button>
+          </InputGroup>
+        </Form>
+
+        {news.length ? (
+          news.map((item, index) => (
+            <Card className="mb-4 w-50 mx-auto" key={index}>
+              <Card.Img variant="top" src={item.image} />
+              <Card.Body>
+                <Card.Title>{item.headline}</Card.Title>
+                <Card.Text>{item.summary}</Card.Text>
+                <Card.Link href={item.url}>Full article</Card.Link>
+              </Card.Body>
+            </Card>
+          ))
+        ) : (
+          <div className="d-flex justify-content-center">
+            <p>Enter a stock symbol to retrieve company news...</p>
           </div>
-        ))
-      ) : (
-        <p>Failed to load news data...</p>
-      )}
+        )}
+      </Container>
     </div>
   );
 }
