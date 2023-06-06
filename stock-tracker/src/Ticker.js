a461aeffea601cffd74a20d375b6311d30b76752
Jacob Bentley <jpb7@pdx.edu>
2023-06-05 17:22:21 -0700

    Gets data, doesn't update in-browser

--- stock-tracker/src/Ticker.js
+++ stock-tracker/src/Ticker.js
@@ -1,53 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
+import useWebSocket from 'react-use-websocket';
 import './ticker.css';
 
-  //  Define stock symbols to be tracked without re-defining every render
-  //const faangSymbols = useMemo(()=> ['AAPL', 'FB', 'AMZN', 'NFLX', 'GOOGL'], []);
-  //const faangSymbols = ['AAPL', 'FB', 'AMZN', 'NFLX', 'GOOGL'];
+//  TODO: update on-screen numbers beyond zero value
 
 const Ticker = () => {
-  //  Configure socket for receiving stock data
-  const { sendMessage, lastMessage, readyState } = useWebSocket(
-    `wss://ws.finnhub.io?token=${process.env.REACT_APP_FINNHUB_API_KEY}`
+  const [tickerData, setTickerData] = useState(
+    [
+      'AAPL', 'FB', 'AMZN', 'NFLX', 'GOOGL', 'MSFT', 'NVDA', 'TSLA', 'INTC',
+      'AMD', 'IBM', 'CRM', 'ORCL', 'ADBE', 'PYPL', 'TWTR', 'SQ', 'CRM', 'SNAP',
+      'SHOP', 'UBER', 'ZM', 'DOCU', 'PTON', 'ROKU', 'NET', 'ROST', 'ATVI', 'EA',
+      'ADSK', 'TEAM', 'OKTA', 'ZS', 'WDAY', 'CRWD', 'NOW', 'TWLO', 'DDOG', 'MELI'
+    ]
+      .map((symbol) => ({
+        symbol,
+        price: 0,
+        //change: Number(),
+        //changePercent: Number(),
+      }))
   );
-  //  TODO: condense this
-  //  TODO: remove dummy values, test when market is open
-  //  Define state to store ticker data
-  const [tickerData, setTickerData] = useState([
-    { symbol: 'AAPL', price: '150.00', change: '-2.50', changePercent: '-1.64' },
-    { symbol: 'FB', price: '350.00', change: '4.25', changePercent: '1.23' },
-    { symbol: 'AMZN', price: '3200.00', change: '-15.75', changePercent: '-0.49' },
-    { symbol: 'NFLX', price: '500.00', change: '8.50', changePercent: '1.73' },
-    { symbol: 'GOOGL', price: '2500.00', change: '-20.25', changePercent: '-0.80' },
-  ]);
-  //  Request data from Finnhub when socket is ready
-  useEffect(() => {
-    if (readyState === ReadyState.OPEN) {
-      tickerData.forEach((stock) => {
-        sendMessage(JSON.stringify({ type: 'subscribe', symbol: stock.symbol }));
-      });
+  //  Configure socket for receiving stock data
+  const { sendMessage, lastMessage } = useWebSocket(
+    `wss://ws.finnhub.io?token=${process.env.REACT_APP_FINNHUB_API_KEY}`,
+    {
+      onOpen: () => {
+        tickerData.forEach((stock) => {
+          sendMessage(JSON.stringify({ type: 'subscribe', symbol: stock.symbol}));
+        });
+      },
+      onMessage: (message) => {
+        console.log('Message:', message);
+        const messageData = JSON.parse(message.data);
+        setTickerData((prevTickerData) => {
+          return prevTickerData.map((stock) => {
+            if (stock.symbol === messageData.s) {
+              return {
+                ...stock,
+                price: Number(messageData.p).toFixed(2),
+                //change: Number((messageData.p - stock.price).toFixed(2)),
+                //changePercent: Number(`${((messageData.p - stock.price) / stock.price * 100).toFixed(2)}%`),
+              };
+            }
+            return stock;
+          });
+        });
+      },
+      onError: (error) => {
+        console.error('WebSocket error:', error);
+      },
+      shouldReconnect: () => true,
     }
-  }, [readyState, sendMessage, tickerData]);
+  );
 
   //  Update ticker data whenever a new message arrives
   useEffect(() => {
     if (lastMessage !== null) {
       console.log('Message:', lastMessage);
-      const messageData = JSON.parse(lastMessage.data);
-      setTickerData((prevTickerData) => {
-        return prevTickerData.map((stock) => {
-          if (stock.symbol === messageData.symbol) {
-            return {
-              ...stock,
-              price: messageData.price.toFixed(2),
-              change: messageData.change.toFixed(2),
-              changePercent: `${messageData.changep.toFixed(2)}%`,
-            };
-          }
-          return stock;
-        });
-      });
     }
   }, [lastMessage]);
 
