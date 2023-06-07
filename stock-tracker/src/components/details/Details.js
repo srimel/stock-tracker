b8518529b057dd0457d5f67753a237129ae065af
Stuart Rimel <stuart.rimel@gmail.com>
2023-06-06 20:30:56 -0700

    create details modal popup

---
+++ stock-tracker/src/components/details/Details.js
@@ -0,0 +1,141 @@
+import './Details.css';
+import React, { useEffect, useState } from 'react';
+import CandleStick from '../chart/CandleStick';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Modal from 'react-bootstrap/Modal';
+
+function Details() {
+  const [show, setShow] = useState(false);
+  const [search, setSearch] = useState('');
+  const [symbol, setSymbol] = useState('');
+  const [metrics, setMetrics] = useState({});
+  const [companyName, setCompanyName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleClose = () => {
+    setShow(false);
+    setSymbol('');
+    setSearch('');
+    setMetrics({});
+    setCompanyName('');
+    setErrorMessage('');
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    setSymbol(search);
+    setShow(true);
+  };
+
+  useEffect(() => {
+    const fetchMetrics = async () => {
+      if (!symbol) {
+        return;
+      }
+      try {
+        const response = await fetch(
+          `https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=all&token=${process.env.REACT_APP_FINNHUB_API_KEY}`,
+        );
+        const data = await response.json();
+        console.log('metricsData', data);
+
+        if (data.metric) {
+          const {
+            '10DayAverageTradingVolume': volume,
+            assetTurnoverAnnual: assetTurnover,
+            bookValuePerShareQuarterly: bookValue,
+            cashFlowPerShareAnnual: cashFlow,
+            grossMarginAnnual: grossMargin,
+            netProfitMarginAnnual: profitMargin,
+            revenuePerShareAnnual: revenuePerShare,
+          } = data.metric;
+
+          setMetrics({
+            volume,
+            assetTurnover,
+            bookValue,
+            cashFlow,
+            grossMargin,
+            profitMargin,
+            revenuePerShare,
+          });
+
+          setCompanyName(data.metric['companyName']);
+        } else {
+          console.error('No metrics found for the given stock symbol.');
+          setErrorMessage(
+            `No metrics found for the given stock symbol: ${symbol}.`,
+          );
+        }
+        const profileResponse = await fetch(
+          `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`,
+        );
+        const profileData = await profileResponse.json();
+
+        console.log('profileData', profileData);
+
+        if (profileData.name) {
+          setCompanyName(profileData.name);
+        } else {
+          console.error('No company name found for the given stock symbol.');
+          setErrorMessage(
+            `No company name found for the given stock symbol: ${symbol}.`,
+          );
+        }
+      } catch (error) {
+        console.error('Error fetching metrics:', error);
+      }
+    };
+    fetchMetrics();
+  }, [symbol]);
+
+  return (
+    <div id="details">
+      <Form className="mb-4" onSubmit={handleSearchSubmit}>
+        <InputGroup className="w-50 mx-auto">
+          <Form.Control
+            type="text"
+            value={search}
+            onChange={handleChange}
+            placeholder="Search by company symbol..."
+          />
+          <Button type="submit">Fetch Metrics</Button>
+        </InputGroup>
+      </Form>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{companyName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {errorMessage === '' ? (
+            <>
+              <p>10-Day Average Trading Volume: {metrics.volume}</p>
+              <p>Asset Turnover (Annual): {metrics.assetTurnover}</p>
+              <p>Book Value per Share (Quarterly): {metrics.bookValue}</p>
+              <p>Cash Flow per Share (Annual): {metrics.cashFlow}</p>
+              <p>Gross Margin (Annual): {metrics.grossMargin}</p>
+              <p>Net Profit Margin (Annual): {metrics.profitMargin}</p>
+              <p>Revenue per Share (Annual): {metrics.revenuePerShare}</p>
+              {symbol && (
+                <div className="d-flex justify-content-center">
+                  <CandleStick symbol1={symbol} />{' '}
+                </div>
+              )}
+            </>
+          ) : (
+            <p className="text-danger">{errorMessage}</p>
+          )}
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default Details;
--- stock-tracker/src/components/home/Home.js
+++ stock-tracker/src/components/home/Home.js
@@ -1,110 +1,25 @@
 import './Home.css';
 import CandleStick from '../chart/CandleStick';
+import Details from '../details/Details';
 
 // Per wireframe:
 // Home page will contain a search input for bringing up the stock details modal.
 // Home page will contain three candlestick charts for common stocks?. Composite indices are premium finnhub features.
 // Home page will coantain a sector map or some other chart thing.
 
-import React, { useState } from 'react';
-
-const Home = () => {
-  const [symbol, setSymbol] = useState('');
-  const [metrics, setMetrics] = useState({});
-  const [companyName, setCompanyName] = useState('');
-
-  const handleChange = (event) => {
-    setSymbol(event.target.value);
-  };
-
-  const fetchMetrics = async () => {
-    try {
-      const response = await fetch(
-        `https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=all&token=${process.env.REACT_APP_FINNHUB_API_KEY}`
-      );
-      const data = await response.json();
-
-      if (data.metric) {
-        const {
-          '10DayAverageTradingVolume': volume,
-          'assetTurnoverAnnual': assetTurnover,
-          'bookValuePerShareQuarterly': bookValue,
-          'cashFlowPerShareAnnual': cashFlow,
-          'grossMarginAnnual': grossMargin,
-          'netProfitMarginAnnual': profitMargin,
-          'revenuePerShareAnnual': revenuePerShare,
-        } = data.metric;
-
-        setMetrics({
-          volume,
-          assetTurnover,
-          bookValue,
-          cashFlow,
-          grossMargin,
-          profitMargin,
-          revenuePerShare,
-        });
-        setCompanyName(data.metric['companyName']);
-      } else {
-        console.error('No metrics found for the given stock symbol.');
-      }
-      const profileResponse = await fetch(
-        `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`
-      );
-      const profileData = await profileResponse.json();
-
-      if (profileData.name) {
-        setCompanyName(profileData.name);
-      } else {
-        console.error('No company name found for the given stock symbol.');
-      }
-    } catch (error) {
-      console.error('Error fetching metrics:', error);
-    }
-  };
-
-  return (
-    <div>
-      <div className = "text-center">
-      <input 
-        type="text"
-        value={symbol}
-        onChange={handleChange}
-        placeholder="Enter stock symbol"
-      />
-    
-      <button type="button" className="btn btn-primary"onClick={fetchMetrics}>Fetch Metrics</button>
-      {companyName && <h2>{companyName}</h2>}
-        <p>10-Day Average Trading Volume: {metrics.volume}</p>
-        <p>Asset Turnover (Annual): {metrics.assetTurnover}</p>
-        <p>Book Value per Share (Quarterly): {metrics.bookValue}</p>
-        <p>Cash Flow per Share (Annual): {metrics.cashFlow}</p>
-        <p>Gross Margin (Annual): {metrics.grossMargin}</p>
-        <p>Net Profit Margin (Annual): {metrics.profitMargin}</p>
-        <p>Revenue per Share (Annual): {metrics.revenuePerShare}</p>
-      {symbol && <div className = "d-flex justify-content-center"><CandleStick symbol1 ={symbol}/> </div> }
-      </div>
-      
-    </div>
-  );
-};
-export default Home;
-
-
-/*
 function Home() {
   return (
     <div className="home">
-      {<SearchInputForStockDetails /> }
+      {/* {<SearchInputForStockDetails />} */}
+      <Details />
       <div className="chart-container">
         <CandleStick symbol1="AAPL" />
         <CandleStick symbol1="TSLA" />
         <CandleStick symbol1="AMZN" />
       </div>
-      { <SectorMap /> }
+      {/* {<SectorMap />} */}
     </div>
   );
 }
 
 export default Home;
-*/
